770b52ecb34b65a8254e1373a4d9c8a23b9bc065
TantowiPutra <tantowi6758@gmail.com>
2025-04-26 06:56:53 +0700

    tugas day-5:: Routing & page Render

---
+++ app/(admin)/blog/[id]/page.tsx
@@ -0,0 +1,40 @@
+'use client' 
+
+// SERVER COMPONENT
+import { useParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+
+// export default async function Blog({ params } : {params : Promise <{ id: string }>}) {
+//     const id = (await params).id;
+
+//     return (
+//         <div>
+//             Blog Detail Page {id}
+//         </div>
+//     );
+// }
+
+// CLIENT COMPONENT
+// export default function Blog() {
+//     const params = useParams();
+
+//     return (
+//         <div>
+//             Blog Detail Page {params.id}
+//         </div>
+//     );
+// }
+
+export default function Blog() {
+    const params = useParams();
+    const searchParams = useSearchParams();
+    const query = Object.fromEntries(searchParams.entries());
+
+    console.log(query)
+
+    return (
+        <div>
+            Blog Detail Page {`dynamic routes: ${params.id}, query param: ${query.nama}, age: ${query.age}`}
+        </div>
+    );
+}
